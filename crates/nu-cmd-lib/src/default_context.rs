0c086816414be84aabf91983ee0d6dd680b03b06
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2023-02-28 12:55:03 -0800

    pull in lots of other directories that are essential to the build process including viewers which pulls in lots of other stuff

--- crates/nu-cmd-lib/src/default_context.rs
+++ crates/nu-cmd-lib/src/default_context.rs
@@ -3,7 +3,8 @@ use nu_protocol::engine::{EngineState, StateWorkingSet};
 use crate::*;
 
 pub fn create_default_context() -> EngineState {
-    let mut engine_state = EngineState::new();
+    //let mut engine_state = EngineState::new();
+    let mut engine_state = nu_cmd_lang::create_default_context();
 
     let delta = {
         let mut working_set = StateWorkingSet::new(&engine_state);
@@ -34,6 +35,11 @@ pub fn create_default_context() -> EngineState {
             External,
             NuCheck,
             Sys,
+        };
+
+        // Viewers
+        bind_command! {
+            Table,
         };
 
         working_set.render()
--- crates/nu-cmd-lib/src/lib.rs
+++ crates/nu-cmd-lib/src/lib.rs
@@ -1,9 +1,15 @@
 mod default_context;
 mod deprecated;
 mod env;
+mod example_test;
 mod system;
+mod viewers;
 
 pub use default_context::*;
 pub use deprecated::*;
 pub use env::*;
 pub use system::*;
+pub use viewers::*;
+
+#[cfg(test)]
+pub use example_test::test_examples;
